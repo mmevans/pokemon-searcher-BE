3cccbeb8311fcecb561229766eaec9a3714179f7
Michael Evans <michael.evans@umbragestudios.com>
2021-01-14 08:11:36 -0600

    change whiteList

--- index.js
+++ index.js
@@ -25,4 +25,23 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://react-node-express-pokemon.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.listen(PORT);
